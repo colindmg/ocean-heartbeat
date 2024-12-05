2fbc69ae9ae7d60532f2c10b0470bf676d454ee5
Col' ✦ <colin.demouge@gmail.com>
2024-12-05 13:02:58 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center w-full mt-10 text-2xl font-bold">
+        ReactJS Boilerplate
+      </h1>
+    </>
+  );
+}
+
+export default App;
