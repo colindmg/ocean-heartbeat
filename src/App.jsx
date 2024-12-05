be331637468b1a8b4943f27f89141ff777186860
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-12-05 17:58:47 +0100

    Installation de r3postprocessing et modif config linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { EffectComposer, WaterEffect } from "@react-three/postprocessing";
+
 function App() {
   return (
     <>
-      <h1 className="text-center w-full mt-10 text-2xl font-bold">
-        ReactJS Boilerplate
-      </h1>
+      <Canvas
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <ambientLight />
+        <directionalLight position={[10, 10, 10]} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+
+        <OrbitControls />
+
+        <EffectComposer>
+          <WaterEffect factor={0.75} />
+        </EffectComposer>
+      </Canvas>
     </>
   );
 }
